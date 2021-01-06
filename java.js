48d98725eab23b2d4300ac5d46f7e810b0e55608
test <test>
2021-01-06 16:45:48 +0500

    test

---
+++ java.js
@@ -0,0 +1,40 @@
+// promt esa savol ornida oldi qisimda otadi
+var nick = prompt(' Ismingiz nima ')
+console.log( nick );
+
+var nun = prompt(' Yoshingiz nechchida');
+console.log(' Mening Yoshim ' + nun + ' da ');
+// alert bu sozlama javob belgileydi yani ichki sistema  
+alert(' Sizning Yoshingiz 18 dan kotsa bolsa juda yaxshi ')
+// javob 54 
+var nun = prompt('MISOL 1: ' + ' 6 * 9 = ? ' )
+console.log(' Misol 1: ' + ' Tog`ri Javob ' + (6 * 9  ) + ' Sizning Javobinggiz ' + nun );
+
+
+// javob 70
+var nun = prompt('MISOL 2: ' + ' 35 + 35 = ? ' )
+console.log(' Misol 2: ' + ' Tog`ri Javob ' + ( 35 + 35  ) + ' Sizning Javobinggiz ' + nun );
+
+
+// javob 10
+var nun = prompt('MISOL 3: ' + ' 100 / 30 = ? ' )
+console.log(' Misol  3: ' + ' Tog`ri Javob ' + ( 100 / 30  ) + ' Sizning Javobinggiz ' + nun );
+
+
+// javob 900
+var nun = prompt('MISOL 4: ' + ' 100 * 9 = ? ' )
+console.log(' Misol 4: ' + ' Tog`ri Javob ' + ( 100 * 9 ) + ' Sizning Javobinggiz ' + nun );
+
+
+// javob 90
+var nun = prompt('MISOL 5: ' + ' 3 * 30  = ? ' )
+console.log(' Misol 5: ' + ' Tog`ri Javob ' + ( 3 * 30  ) + ' Sizning Javobinggiz ' + nun );
+
+
+// javob 100
+var nun = prompt('MISOL 6: ' + ' 400 - 200 - 100 = ? ' )
+console.log(' Misol 6: ' + ' Tog`ri Javob ' + ( 400 - 200 - 100  ) + ' Sizning Javobinggiz ' + nun );
+
+// javoblar korish uchun 
+alert(' Javoblarni korish uchun " CONSOLGA " oting  ')
+
